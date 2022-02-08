ed5d77fa1667b3703876fd0489d45b04f905e693
Evelinatoleike <ejanusauskaite@gmail.com>
2022-02-08 22:59:56 +0200

    5 uzduotis

---
+++ komentaras/main.js
@@ -0,0 +1,13 @@
+let bnt1 = document.getElementById("btn1");
+btn1.addEventListener("click", function () {
+    let form = document.getElementById("form");
+    form.style.display = "block";
+});
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function (e) {
+    e.preventDefault();
+    swal("Dėkojame už jūsų nuomonę!", "Gražios dienos", "success");
+    let form = document.getElementById("form");
+    form.style.display = "none";
+
+});
