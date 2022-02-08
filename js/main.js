029f78218535a827769442ad82c9bac7dc626022
Evelinatoleike <ejanusauskaite@gmail.com>
2022-02-08 21:48:41 +0200

    button ready

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let btn1 = document.getElementById("btn1");
+
+btn1.addEventListener("click", function () {
+    let p = document.querySelector("p");
+    p.style.color = "yellow";
+})
+
+let btn2 = document.getElementById("btn2");
+
+btn2.addEventListener("click", function () {
+    let p = document.querySelector("p");
+    p.style.color = "green";
+})
+
+let btn3 = document.getElementById("btn3");
+
+btn3.addEventListener("click", function () {
+    let p = document.querySelector("p");
+    p.style.color = "red";
+})
