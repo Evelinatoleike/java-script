487028f46c10db5172d43880350995f89e0f0070
Evelinatoleike <ejanusauskaite@gmail.com>
2022-02-12 12:17:34 +0200

    json

--- img-text/main.js
+++ img-text/main.js
@@ -1,15 +1,15 @@
 let dataArr = [
     {
         imgSrc: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg",
-        text: "Wonderful you"
+        text: "you"
     },
     {
         imgSrc: "https://cdn.pixabay.com/photo/2022/01/15/02/07/windows-6938478_960_720.jpg",
-        text: "Wonderful day"
+        text: "day"
     },
     {
         imgSrc: "https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509_960_720.jpg",
-        text: "Wonderful time"
+        text: "time"
     }
 ];
 
@@ -18,7 +18,7 @@ let btn = document.querySelector("button");
 function changeHtml(data) {
     document.querySelector("img").setAttribute("src", data.imgSrc);
     let text = document.querySelector(".img-text");
-    text.innterHTML = data.text;
+    text.innerHTMT = data.text;
 }
 
 function randomIndex(size) {
