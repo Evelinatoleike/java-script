76f66a2447667492bdaccd301a1ebb1773569902
Evelinatoleike <ejanusauskaite@gmail.com>
2022-02-16 13:22:28 +0200

    json

--- img-text/main.js
+++ img-text/main.js
@@ -1,17 +1,17 @@
-//let dataArr = [
-//  {
-//      imgSrc: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg",
-//       text: "you"
-//   },
-//   {
-//      imgSrc: "https://cdn.pixabay.com/photo/2022/01/15/02/07/windows-6938478_960_720.jpg",
-//      text: "day"
-//  },
-//  {
-//       imgSrc: "https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509_960_720.jpg",
-//       text: "time"
-//   }
-//];
+// let dataArr = [
+//     {
+//         imgSrc: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_960_720.jpg",
+//         text: "You are beautiful"
+//     },
+//     {
+//         imgSrc: "https://cdn.pixabay.com/photo/2022/01/15/02/07/windows-6938478_960_720.jpg",
+//         text: "Day is beautiful"
+//     },
+//     {
+//         imgSrc: "https://cdn.pixabay.com/photo/2015/04/19/08/32/rose-729509_960_720.jpg",
+//         text: "Time is beautiful"
+//     }
+// ];
 
 let btn = document.querySelector("button");
 
@@ -26,18 +26,20 @@ function randomIndex(size) {
     return randomInd;
 }
 
-btn.addEventListener("click", e => {
-    let randInd = randomIndex(dataArr.length);
-    changeHtml(dataArr[randInd]);
-})
+// btn.addEventListener("click", e => {
+//     let randInd = randomIndex(dataArr.length);
+//     changeHtml(dataArr[randInd]);
+// })
 
 
 
 async function getData() {
-    const requestURL = 'https://raw.githubusercontent.com/Evelinatoleike/java-script/main/data.json';
+    const requestURL = 'https://raw.githubusercontent.com/Evelinatoleike/json/main/data.json';
     const request = new Request(requestURL);
+
     const response = await fetch(request);
     const JSON = await response.json();
+
     console.log(JSON);
     changeHtml(JSON[randomIndex(JSON.length)]);
 }
